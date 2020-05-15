8504e67fca4ee2028f884f356f4478299b8462b1
Alex Disney <alexdisney@gmail.com>
2020-05-15 16:07:01 -0500

    Add a blacklist with higher priority than whitelist
    In cases where it is desireable to log objects with many keys, mostly
    whitelisted but with a few exceptions nested deep in the object, the
    whitelist can become large and repetitive. This is resolved by adding a
    blacklist allowing wildcarding, allowing anonymization of single keys,
    deeply nested.
    
    For defensive purposes, the blacklist should be prioritized more highly
    than the whitelist. The assumption here is that it is of great
    importance to anonymize an item placed on the blacklist. This also
    prevents the class of errors in which items with wildcards are added to
    the whitelist after initial implementation, causing sensitive data to be
    logged despite being present on the blacklist.
    
    One side effect or gotcha is that keys that are a super string of blacklisted
    items with wildcards will be redacted regardless of their presence on
    the whitelist and not explicitely being thought of for blacklisting
    (i.e. the motivation for adding `toAnonymize.*` is to recursively
    redact values under the `toAnonymize` key rather than keys of the type
    `toAnonymizeSuperString`).
    
    https://github.com/uphold/anonymizer/issues/27

--- src/index.js
+++ src/index.js
@@ -17,9 +17,11 @@ const replacement = '--REDACTED--';
  * Module exports.
  */
 
-module.exports = (whitelist = []) => {
-  const terms = whitelist.join('|');
-  const paths = new RegExp(`^(${terms.replace('.', '\\.').replace(/\*/g, '.*')})$`, 'i');
+module.exports = ({ blacklist = [], whitelist = [] } = {}) => {
+  const whitelistTerms = whitelist.join('|');
+  const whitelistPaths = new RegExp(`^(${whitelistTerms.replace('.', '\\.').replace(/\*/g, '.*')})$`, 'i');
+  const blacklistTerms = blacklist.join('|');
+  const blacklistPaths = new RegExp(`^(${blacklistTerms.replace('.', '\\.').replace(/\*/g, '.*')})$`, 'i');
 
   return values => {
     const obj = clone(values);
@@ -29,17 +31,18 @@ module.exports = (whitelist = []) => {
         return '[Circular ~]';
       }
 
+      const path = this.path.join('.');
       const isBuffer = this.node instanceof Buffer;
 
       if (!isBuffer && !this.isLeaf) {
         return;
       }
 
-      if (isBuffer && paths.test(this.path.join('.'))) {
+      if (isBuffer && (!blacklistPaths.test(path) && whitelistPaths.test(path))) {
         return this.update(this.node, true);
       }
 
-      if (!paths.test(this.path.join('.'))) {
+      if (blacklistPaths.test(path) || !whitelistPaths.test(path)) {
         this.update(replacement);
       }
     });
