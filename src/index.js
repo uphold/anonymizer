cd3f913f1d4d0330eb004f4b530a41b63a615990
Miguel Martins <mmartins@uphold.com>
2021-02-05 17:28:03 +0000

    Replace traverse with custom implementation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@
  * Module dependencies.
  */
 
-const get = require('lodash.get');
-const stringify = require('json-stringify-safe');
-const traverse = require('traverse');
+const isArray = require('lodash.isarray');
+const isBuffer = require('lodash.isbuffer');
+const isDate = require('lodash.isdate');
+const isObject = require('lodash.isobject');
+const isRegExp = require('lodash.isregexp');
 
 /**
  * Constants.
@@ -15,37 +17,72 @@ const traverse = require('traverse');
 const DEFAULT_REPLACEMENT = '--REDACTED--';
 
 /**
+ * Check if value is a leaf.
+ */
+
+const isLeaf = value => !isObject(value) || isDate(value) || isRegExp(value) || isBuffer(value);
+
+/**
+ * Append key to path.
+ */
+
+const appendToPath = (path, key) => {
+  return path !== null ? `${path}.${key}` : `${key}`;
+};
+
+/**
+ * Traverse.
+ */
+
+function traverse(element, transformer = value => value) {
+  function walk(value, key, path, cycleDetector) {
+    if (isLeaf(value)) {
+      return transformer(value, key.toString(), path);
+    }
+
+    if (cycleDetector.has(value)) {
+      return '[Circular ~]';
+    }
+
+    cycleDetector.add(value);
+
+    if (value.toJSON) {
+      value = value.toJSON();
+    }
+
+    if (isArray(value)) {
+      return value.map((value, key) => walk(value, key, appendToPath(path, key), new Set(cycleDetector)));
+    }
+
+    const result = {};
+
+    for (const property in value) {
+      result[property] = walk(value[property], property, appendToPath(path, property), new Set(cycleDetector));
+    }
+
+    return result;
+  }
+
+  return walk(element, null, null, new Set());
+}
+
+/**
  * Module exports.
  */
 
 module.exports = ({ blacklist = [], whitelist = [] } = {}, { replacement = () => DEFAULT_REPLACEMENT } = {}) => {
-  const whitelistTerms = whitelist.join('|');
-  const whitelistPaths = new RegExp(`^(${whitelistTerms.replace(/\./g, '\\.').replace(/\*/g, '.*')})$`, 'i');
   const blacklistTerms = blacklist.join('|');
   const blacklistPaths = new RegExp(`^(${blacklistTerms.replace(/\./g, '\\.').replace(/\*/g, '.*')})$`, 'i');
+  const whitelistTerms = whitelist.join('|');
+  const whitelistPaths = new RegExp(`^(${whitelistTerms.replace(/\./g, '\\.').replace(/\*/g, '.*')})$`, 'i');
 
   return values => {
-    const obj = JSON.parse(stringify(values));
-
-    traverse(obj).forEach(function() {
-      const path = this.path.join('.');
-      const isBuffer = Buffer.isBuffer(get(values, path));
-
-      if (!isBuffer && !this.isLeaf) {
-        return;
+    return traverse(values, (node, key, path) => {
+      if (!whitelistPaths.test(path) || blacklistPaths.test(path)) {
+        return replacement(key, node, path);
       }
 
-      if (isBuffer && (!blacklistPaths.test(path) && whitelistPaths.test(path))) {
-        return this.update(Buffer.from(this.node), true);
-      }
-
-      const replacedValue = replacement(this.key, this.node, this.path);
-
-      if (blacklistPaths.test(path) || !whitelistPaths.test(path)) {
-        this.update(replacedValue);
-      }
+      return node;
     });
-
-    return obj;
   };
 };
