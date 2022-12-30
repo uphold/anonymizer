dc29a7199953d29dd9c58190062fbdb714767113
Diogo Sobral <diogo.sobral@uphold.com>
2022-12-30 12:55:34 +0000

    Add support for serializers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@
  * Module dependencies.
  */
 
+const { serializeError } = require('serialize-error');
+const cloneDeep = require('lodash.clonedeep');
+const cloneDeepWith = require('lodash.clonedeepwith');
 const get = require('lodash.get');
+const set = require('lodash.set');
 const stringify = require('json-stringify-safe');
 const traverse = require('traverse');
 
@@ -15,17 +19,110 @@ const traverse = require('traverse');
 const DEFAULT_REPLACEMENT = '--REDACTED--';
 
 /**
- * Module exports.
+ * Gets a list with all properties of an object.
  */
 
-module.exports = (
+function getAllPropertyNames(obj, maxChainLength = 10) {
+  const set = new Set();
+  let i = 0;
+
+  while (obj.constructor !== Object && i < maxChainLength) {
+    Object.getOwnPropertyNames(obj).forEach(name => set.add(name));
+    obj = Object.getPrototypeOf(obj);
+    i++;
+  }
+
+  return [...set];
+}
+
+/**
+ * Custom error clone.
+ */
+
+function customErrorClone(error) {
+  const clone = {};
+
+  getAllPropertyNames(error).forEach(key => {
+    const isConstructor = key === 'constructor';
+    const isFunction = typeof error[key] === 'function';
+
+    if (!isConstructor && !isFunction) {
+      clone[key] = cloneDeep(error[key]);
+    }
+  });
+
+  return clone;
+}
+
+/**
+ * Validate serializers.
+ */
+
+function validateSerializers(serializers) {
+  serializers.forEach(({ path, serializer }) => {
+    if (typeof serializer !== 'function') {
+      throw new TypeError(`Invalid serializer for \`${path}\` path: must be a function`);
+    }
+  });
+}
+
+/**
+ * `parseAndSerialize` builds a copy of the original object, computes the serializer's results and mutates the
+ * copy with the serialized values. To perform the copy, we are using JSON.parse(stringify(values)), which does
+ * not construct an exact replication of the initial input, but it can't be swapped for another solution due to
+ * its performance and because it can also handle classes correctly. While most of the existing deep clones, when
+ * cloning Classes receive a `Class` as input and return a `Class` as output, `stringify` does things differently.
+ * When it receives a `Class`, it calls the method `toJSON` and returns its output or if the `Class` doesn't have
+ * `toJSON` it returns an empty object. This is very powerful since `traverse` can't iterate over over `Classes`.
+ * During `parseAndSerialize` execution, we perform additional copies to avoid having a serializer updating the
+ * original object by reference. These copies are only done in the values passed to serializers to avoid two full
+ * copies of the original values. For this, we used `cloneDeepWith` with a custom clone only for errors. When an
+ * error is found, we compute a list with all properties (properties from the class itself and from extended cla-
+ * sses). Then we use these properties to get the original values and copying them into a new object.
+ */
+
+function parseAndSerialize(values, serializers) {
+  const target = JSON.parse(stringify(values));
+
+  for (const { path, serializer } of serializers) {
+    const value = get(values, path);
+
+    if (value === undefined) {
+      continue;
+    }
+
+    try {
+      const copy = cloneDeepWith(value, node => {
+        if (node instanceof Error) {
+          return customErrorClone(node);
+        }
+
+        return undefined;
+      });
+
+      set(target, path, serializer(copy));
+    } catch (error) {
+      set(target, path, `Anonymize ERROR: Error while applying ${path} serializer`);
+    }
+  }
+
+  return target;
+}
+
+/**
+ * Module exports `anonymizer` function.
+ */
+
+module.exports.anonymizer = (
   { blacklist = [], whitelist = [] } = {},
-  { replacement = () => DEFAULT_REPLACEMENT, trim = false } = {}
+  { replacement = () => DEFAULT_REPLACEMENT, serializers = [], trim = false } = {}
 ) => {
   const whitelistTerms = whitelist.join('|');
   const whitelistPaths = new RegExp(`^(${whitelistTerms.replace(/\./g, '\\.').replace(/\*/g, '.*')})$`, 'i');
   const blacklistTerms = blacklist.join('|');
   const blacklistPaths = new RegExp(`^(${blacklistTerms.replace(/\./g, '\\.').replace(/\*/g, '.*')})$`, 'i');
+
+  validateSerializers(serializers);
 
   return values => {
     if (!(values instanceof Object)) {
@@ -33,7 +130,7 @@ module.exports = (
     }
 
     const blacklistedKeys = new Set();
-    const obj = JSON.parse(stringify(values));
+    const obj = parseAndSerialize(values, serializers);
 
     traverse(obj).forEach(function() {
       const path = this.path.join('.');
@@ -51,7 +148,7 @@ module.exports = (
         return;
       }
 
-      if (isBuffer && (!blacklistPaths.test(path) && whitelistPaths.test(path))) {
+      if (isBuffer && !blacklistPaths.test(path) && whitelistPaths.test(path)) {
         return this.update(Buffer.from(this.node), true);
       }
 
@@ -77,4 +174,24 @@ module.exports = (
 
     return obj;
   };
+};
+
+/**
+ * Default serializer for Datadog.
+ */
+
+function datadogSerializer(error) {
+  return {
+    ...error,
+    kind: error.name || 'Error'
+  };
+}
+
+/**
+ * Module exports `defaultSerializers`.
+ */
+
+module.exports.defaultSerializers = {
+  datadogSerializer,
+  error: serializeError
 };
--- test/src/index.test.js
+++ test/src/index.test.js
@@ -4,7 +4,12 @@
  * Module dependencies.
  */
 
-const anonymizer = require('src');
+const {
+  anonymizer,
+  defaultSerializers: { datadogSerializer }
+} = require('src');
+const { generateObjectSample, generateObjectSamplePaths } = require('./benchmark/samples');
+const { serializeError } = require('serialize-error');
 
 /**
  * Test `Anonymizer`.
@@ -251,6 +256,292 @@ describe('Anonymizer', () => {
       });
     });
 
+    describe('serializers', () => {
+      it('should throw an error when serializer is not a function', () => {
+        const serializers = [{ path: 'foo', serializer: 123 }];
+        const whitelist = ['*'];
+
+        try {
+          anonymizer({ whitelist }, { serializers });
+
+          fail();
+        } catch (error) {
+          expect(error).toBeInstanceOf(TypeError);
+          expect(error.message).toEqual('Invalid serializer for `foo` path: must be a function');
+        }
+      });
+
+      it('should apply serializers to existing paths', () => {
+        const foobar = jest.fn(() => 'bii');
+        const foobiz = jest.fn(() => 'bzz');
+        const foobzz = jest.fn(() => ({ bar: 'biz' }));
+        const whitelist = ['*'];
+        const serializers = [
+          { path: 'bar', serializer: foobiz },
+          { path: 'foo', serializer: foobar },
+          { path: 'foobar', serializer: foobzz }
+        ];
+        const anonymize = anonymizer({ whitelist }, { serializers });
+
+        const result = anonymize({ foo: 'bar' });
+
+        expect(foobar).toHaveBeenCalledTimes(1);
+        expect(foobar).toHaveBeenCalledWith('bar');
+        expect(foobiz).toHaveBeenCalledTimes(0);
+        expect(foobzz).toHaveBeenCalledTimes(0);
+        expect(result.foo).toEqual('bii');
+      });
+
+      it('should apply serializers to nested paths', () => {
+        const error = new Error('foobar');
+        const foobar = jest.fn(() => 'bii');
+        const foobiz = jest.fn(() => 'bzz');
+        const fooerror = jest.fn(serializeError);
+        const whitelist = ['*'];
+        const serializers = [
+          { path: 'bar.foo', serializer: foobiz },
+          { path: 'bar.error', serializer: fooerror },
+          { path: 'foo.bar.biz', serializer: foobar }
+        ];
+        const anonymize = anonymizer({ whitelist }, { serializers });
+
+        const result = anonymize({
+          bar: { error, foo: 'bar' },
+          foo: {
+            bar: { biz: 'foo' }
+          }
+        });
+
+        expect(foobar).toHaveBeenCalledTimes(1);
+        expect(foobar).toHaveBeenCalledWith('foo');
+        expect(foobiz).toHaveBeenCalledTimes(1);
+        expect(foobiz).toHaveBeenCalledWith('bar');
+        expect(result.bar.foo).toEqual('bzz');
+        expect(result.bar.error).toHaveProperty('name', 'Error');
+        expect(result.bar.error).toHaveProperty('message', 'foobar');
+        expect(result.foo).toEqual({ bar: { biz: 'bii' } });
+      });
+
+      it('should not change original values by reference', () => {
+        const data = { foo: 'bar', foz: { baz: 'baz', biz: 'biz' } };
+        const foobar = jest.fn(() => 'bii');
+        const fozbar = jest.fn(value => {
+          value.baz = 'biz';
+
+          return 'biz';
+        });
+        const whitelist = ['*'];
+        const serializers = [{ path: 'foo', serializer: foobar }, { path: 'foz', serializer: fozbar }];
+        const anonymize = anonymizer({ whitelist }, { serializers });
+
+        const result = anonymize(data);
+
+        expect(data).toEqual({ foo: 'bar', foz: { baz: 'baz', biz: 'biz' } });
+        expect(foobar).toHaveBeenCalledTimes(1);
+        expect(foobar).toHaveBeenCalledWith('bar');
+        expect(result.foo).toEqual('bii');
+        expect(result.foz).toEqual('biz');
+      });
+
+      it('should serialize errors that extend `Error`', () => {
+        class ValidationError extends Error {
+          constructor(message) {
+            super(message);
+            this.name = 'ValidationError';
+            this.foo = 'bar';
+          }
+        }
+
+        const error = new ValidationError('foobar');
+        const serializer = jest.fn(datadogSerializer);
+        const serializers = [{ path: 'error', serializer }];
+        const whitelist = ['error.foo', 'error.kind', 'error.message', 'error.name', 'error.stack'];
+        const anonymize = anonymizer({ whitelist }, { serializers });
+
+        const result = anonymize({ error });
+
+        expect(serializer).toHaveBeenCalledTimes(1);
+        expect(result.error).toEqual({
+          foo: 'bar',
+          kind: 'ValidationError',
+          message: 'foobar',
+          name: 'ValidationError',
+          stack: error.stack
+        });
+      });
+
+      it('should serialize errors that extend multiple classes', () => {
+        class ErrorOne extends Error {
+          constructor(message) {
+            super(message);
+            this.name = 'ErrorOne';
+            this.foo = 'bar';
+          }
+        }
+
+        class ErrorTwo extends ErrorOne {
+          constructor(message) {
+            super(message);
+            this.name = 'ErrorTwo';
+            this.foo = 'baz';
+          }
+        }
+
+        const error = new ErrorTwo('foobar');
+        const serializer = jest.fn(datadogSerializer);
+        const serializers = [{ path: 'error', serializer }];
+        const whitelist = ['error.foo', 'error.kind', 'error.message', 'error.name', 'error.stack'];
+        const anonymize = anonymizer({ whitelist }, { serializers });
+
+        const result = anonymize({ error });
+
+        expect(serializer).toHaveBeenCalledTimes(1);
+        expect(result.error).toEqual({
+          foo: 'baz',
+          kind: 'ErrorTwo',
+          message: 'foobar',
+          name: 'ErrorTwo',
+          stack: error.stack
+        });
+      });
+
+      it('should not change original values of serialized errors', () => {
+        class ValidationError extends Error {
+          constructor(message) {
+            super(message);
+            this.name = 'ValidationError';
+            this.foo = { for: 'baz' };
+          }
+        }
+
+        const error = new ValidationError('foobar');
+        const serializer = jest.fn(value => {
+          const serialized = datadogSerializer(value);
+
+          value.foo.for = 'bar';
+
+          return serialized;
+        });
+        const serializers = [{ path: 'error', serializer }];
+        const whitelist = ['*'];
+        const anonymize = anonymizer({ whitelist }, { serializers });
+
+        const result = anonymize({ error });
+
+        expect(serializer).toHaveBeenCalledTimes(1);
+        expect(result.error).toEqual({
+          foo: { for: 'bar' },
+          kind: 'ValidationError',
+          message: 'foobar',
+          name: 'ValidationError',
+          stack: error.stack
+        });
+        expect(error.foo.for).toEqual('baz');
+      });
+
+      it('should not change deep original values of serialized errors', () => {
+        class ValidationError extends Error {
+          constructor(message) {
+            super(message);
+            this.name = 'ValidationError';
+            this.foo = { for: { bar: { foo: 'baz' } } };
+          }
+        }
+
+        const error = new ValidationError('foobar');
+        const serializer = jest.fn(value => {
+          const serialized = datadogSerializer(value);
+
+          value.foo.for.bar.foo = 'bar';
+
+          return serialized;
+        });
+        const serializers = [{ path: 'error', serializer }];
+        const whitelist = ['*'];
+        const anonymize = anonymizer({ whitelist }, { serializers });
+
+        const result = anonymize({ error });
+
+        expect(serializer).toHaveBeenCalledTimes(1);
+        expect(result.error).toEqual({
+          foo: { for: { bar: { foo: 'bar' } } },
+          kind: 'ValidationError',
+          message: 'foobar',
+          name: 'ValidationError',
+          stack: error.stack
+        });
+        expect(error.foo.for.bar.foo).toEqual('baz');
+      });
+
+      describe('defaultSerializers', () => {
+        it('should serialize errors when `serializeError()` is applied', () => {
+          const error = new Error('foobar');
+          const serializer = jest.fn(serializeError);
+          const serializers = [{ path: 'e', serializer }, { path: 'err', serializer }, { path: 'error', serializer }];
+          const whitelist = ['*'];
+          const anonymize = anonymizer({ whitelist }, { serializers });
+
+          const result = anonymize({
+            e: error,
+            err: {
+              statusCode: 400
+            },
+            error,
+            error2: error,
+            foo: 'bar'
+          });
+
+          expect(serializer).toHaveBeenCalledTimes(3);
+          expect(result.e).toEqual({
+            message: 'foobar',
+            name: 'Error',
+            stack: error.stack
+          });
+          expect(result.err).toEqual({
+            statusCode: 400
+          });
+          expect(result.error).toEqual({
+            message: 'foobar',
+            name: 'Error',
+            stack: error.stack
+          });
+          expect(result.error2).toEqual({});
+          expect(result.foo).toEqual('bar');
+        });
+
+        it('should serialize errors when `datadogSerializer()` is applied', () => {
+          const error = new Error('foobar');
+          const serializer = jest.fn(datadogSerializer);
+          const serializers = [{ path: 'err', serializer }, { path: 'error', serializer }];
+          const whitelist = ['error.foo', 'error.kind', 'error.message', 'error.name', 'error.stack'];
+          const anonymize = anonymizer({ whitelist }, { serializers });
+
+          const result = anonymize({
+            e: error,
+            err: {
+              statusCode: 400
+            },
+            error,
+            error2: error,
+            foo: 'bar'
+          });
+
+          expect(serializer).toHaveBeenCalledTimes(2);
+          expect(result.err).toEqual({
+            kind: '--REDACTED--',
+            statusCode: '--REDACTED--'
+          });
+          expect(result.error).toEqual({
+            kind: 'Error',
+            message: 'foobar',
+            name: 'Error',
+            stack: error.stack
+          });
+          expect(result.foo).toEqual('--REDACTED--');
+        });
+      });
+    });
+
     describe('trim', () => {
       it('should group array keys', () => {
         const anonymize = anonymizer({ whitelist: ['foo'] }, { trim: true });
@@ -308,6 +599,56 @@ describe('Anonymizer', () => {
           buz: '--HIDDEN--',
           foo: 'bar'
         });
+      });
+    });
+
+    describe.skip('benchmark', () => {
+      it('should run a sample with `32768` properties in less than `150` ms', () => {
+        const depth = 10;
+        const data = generateObjectSample({ depth });
+        const anonymize = anonymizer({ blacklist: ['*'] });
+        const startTime = process.hrtime();
+
+        anonymize(data);
+
+        const endTime = process.hrtime(startTime);
+        const msElapsed = endTime[1] / 1000000;
+
+        expect(msElapsed).toBeLessThan(150);
+      });
+
+      it('should call serializers in all `32768` properties in less than `250` ms', () => {
+        const depth = 10;
+        const data = generateObjectSample({ depth });
+        const serializer = jest.fn(() => 'bii');
+        const serializers = generateObjectSamplePaths({ depth }).map(path => ({ path, serializer }));
+        const anonymize = anonymizer({ blacklist: ['*'] }, { serializers });
+        const startTime = process.hrtime();
+
+        anonymize(data);
+
+        const endTime = process.hrtime(startTime);
+        const msElapsed = endTime[1] / 1000000;
+
+        expect(msElapsed).toBeLessThan(250);
+        expect(serializer).toHaveBeenCalledTimes(32768);
+      });
+
+      it('should call `serializeError` in all `32768` properties in less than `175` ms', () => {
+        const depth = 10;
+        const data = generateObjectSample({ depth, leafValue: () => new Error('foobar') });
+        const serializer = jest.fn(serializeError);
+        const serializers = generateObjectSamplePaths({ depth }).map(path => ({ path, serializer }));
+        const anonymize = anonymizer({ blacklist: ['*'] }, { serializers });
+        const startTime = process.hrtime();
+
+        anonymize(data);
+
+        const endTime = process.hrtime(startTime);
+        const msElapsed = endTime[1] / 1000000;
+
+        expect(msElapsed).toBeLessThan(175);
+        expect(serializer).toHaveBeenCalledTimes(32768);
       });
     });
   });
